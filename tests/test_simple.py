81ed43fed169c0a104c47636d621a5e3da8e616e
take-cheeze <takechi101010@gmail.com>
2026-02-27 00:49:13 +0900

    Add test for #33

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,24 +2,37 @@ import torch
 import onnx
 import onnxsim
 
+from .test_python_api import export_simplify_and_check_by_python_api
+
 
 def test_onnx_simplifier():
-    def _create_dummy_model():
-        class MockModel(torch.nn.Module):
-            def __init__(self):
-                super(MockModel, self).__init__()
-                self.linear = torch.nn.Linear(10, 5)
+    class MockModel(torch.nn.Module):
+        def __init__(self):
+            super(MockModel, self).__init__()
+            self.linear = torch.nn.Linear(10, 5)
 
-            def forward(self, x):
-                return self.linear(x)
+        def forward(self, x):
+            return self.linear(x)
 
-        model = MockModel()
-        dummy_input = torch.randn(1, 10)
-        onnx_file = "dummy_model.onnx"
-        torch.onnx.export(model, dummy_input, onnx_file)
-        return onnx_file
+    export_simplify_and_check_by_python_api(MockModel(), torch.randn(1, 10))
 
-    onnx_model_path = _create_dummy_model()
-    onnx_model = onnx.load(onnx_model_path)
-    simple_model, _ = onnxsim.simplify(onnx_model, perform_optimization=False, skip_shape_inference=True)
-    assert simple_model is not None
+def test_mg():
+    class MG(torch.nn.Module):
+
+        def __init__(self):
+            super().__init__()
+
+        def forward(self, x, b):
+            x = x.float()
+            b = b.float()
+            sh = torch.tensor(x.shape)
+            x = x.view(sh[0], sh[1], -1)
+            b = b.squeeze(-1)
+            b = b.squeeze(-1)
+            a = torch.matmul(b, x)
+            preds = a.view(1, 100, sh[2], sh[3])
+            return preds
+
+    x = torch.randn([1, 256, 160, 184])
+    b = torch.randn([100, 256, 1, 1])
+    export_simplify_and_check_by_python_api(MG(), (x, b))
